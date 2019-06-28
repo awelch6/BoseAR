f7d5f4682e5b48ad7550c9e04bcf571c99ce7552
Austin Welch <austinwelch@Austins-MacBook-Pro.local>
2019-06-28 09:43:58 -0400

    initial commit

---
+++ BoseAR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoseAR
+//
+//  Created by Austin Welch on 6/28/19.
+//  Copyright © 2019 Austin Welch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BoseARTests/BoseARTests.swift
@@ -0,0 +1,34 @@
+//
+//  BoseARTests.swift
+//  BoseARTests
+//
+//  Created by Austin Welch on 6/28/19.
+//  Copyright © 2019 Austin Welch. All rights reserved.
+//
+
+import XCTest
+@testable import BoseAR
+
+class BoseARTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
